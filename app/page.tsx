f1b265f4399a4115f6da3f929c8347dda0b85ca3
Bhupit <weebwatcher07@gmail.com>
2026-05-12 09:11:26 +0530

    clean file strucutre

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-5xl font-bold">
+        My Website is Live 🚀
+      </h1>
+    </main>
+  );
+}
